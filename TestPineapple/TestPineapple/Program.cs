2f02fa1ac791f7232edec9932bec3506021654cf
azabiw <ozzivanhala@gmail.com>
2017-12-02 00:24:36 +0200

    kommentit
    Lisättiin tyylikkäät kommentit aliohjelmaan ja pääohjelmaan.

--- TestPineapple/TestPineapple/Program.cs
+++ TestPineapple/TestPineapple/Program.cs
@@ -8,13 +8,20 @@ namespace TestPineapple
 {
     class Program
     {
+        /// <summary>
+        /// Tää on hieno pääohjelma
+        /// </summary>
+        /// <param name="args">turhaa paskaa</param>
         static void Main(string[] args)
         {
             Hullo();
         }
+        /// <summary>
+        /// Tulostaa näytölle hullo
+        /// </summary>
         static void Hullo()
         {
-            Console.WriteLine("Derp");
+            Console.WriteLine("Derp"); //eeppinen kommentti
         }
 
     }
